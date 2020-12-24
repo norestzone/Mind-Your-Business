59d50b4d77d21474b78e9393af4284d2059f81e5
ChassyAHustla <chassyahustla@ChassyAHustlas-MacBook-Pro.local>
2020-12-23 16:47:16 -0800

    pop fixed

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 document.addEventListener("DOMContentLoaded", (e) => {
+    closeButton.addEventListener("click", closePopUp)
 
-    
 })
+
+function closePopUp(){
+    introPopup.style.display="none";
+}
