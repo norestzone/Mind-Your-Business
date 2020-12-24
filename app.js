28fdbbd01a0aabfec2b222547e80a69d21bde245
ChassyAHustla <chassyahustla@ChassyAHustlas-MacBook-Pro.local>
2020-12-23 19:01:54 -0800

    updated decision functions

--- app.js
+++ app.js
@@ -6,3 +6,16 @@ document.addEventListener("DOMContentLoaded", (e) => {
 function closePopUp(){
     introPopup.style.display="none";
 }
+
+// Phase 1
+const phaseOne = {
+    decisionOne: {
+        goodDecision: {
+            text: "You've made a good decision!", destination: "Phase 3"
+        },
+        badDecision: "Oof, bad choice bro...",
+        // What else should go here?
+    }
+}
+
+console.log(phaseOne.decisionOne.goodDecision.text)
