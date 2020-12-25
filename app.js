24c83f453bb3179e291b286ed9daf48b80be6680
ChassyAHustla <chassyahustla@ChassyAHustlas-MacBook-Pro.local>
2020-12-24 20:59:53 -0800

    added phases

--- app.js
+++ app.js
@@ -11,6 +11,44 @@ function closePopUp(){
 const phaseOne = {
     decisionOne: {
         goodDecision: {
+            text: "You've made a good decision!", destination: "Phase 2"
+        },
+        badDecision: {
+            text: "Oof, bad choice bro...",destination: "Phase 2"
+        }
+        // What else should go here?
+    }
+}
+const phaseTwo = {
+    decisionTwo: {
+        goodDecision: {
+            text: "You've made a good decision!", destination: "Phase 3"
+        },
+        badDecision: "Oof, bad choice bro...",
+        // What else should go here?
+    }
+}
+const phaseThree = {
+    decisionThree: {
+        goodDecision: {
+            text: "You've made a good decision!", destination: "Phase 3"
+        },
+        badDecision: "Oof, bad choice bro...",
+        // What else should go here?
+    }
+}
+const phaseFour = {
+    decisionFour: {
+        goodDecision: {
+            text: "You've made a good decision!", destination: "Phase 3"
+        },
+        badDecision: "Oof, bad choice bro...",
+        // What else should go here?
+    }
+}
+const phaseFive = {
+    decisionFive: {
+        goodDecision: {
             text: "You've made a good decision!", destination: "Phase 3"
         },
         badDecision: "Oof, bad choice bro...",
