b17c15fe8c0dbe4ca05f6578e09ba1179f9ba4f4
ChassyAHustla <chassyahustla@ChassyAHustlas-MacBook-Pro.local>
2020-12-27 14:56:27 -0800

    added game phases

--- app.js
+++ app.js
@@ -14,44 +14,60 @@ const phaseOne = {
             text: "You've made a good decision!", destination: "Phase 2"
         },
         badDecision: {
-            text: "Oof, bad choice bro...",destination: "Phase 2"
+            text: "Oof, bad choice bro...", destination: "Phase 2"
         }
         // What else should go here?
     }
 }
+
+// Phase 2
 const phaseTwo = {
     decisionTwo: {
         goodDecision: {
             text: "You've made a good decision!", destination: "Phase 3"
         },
-        badDecision: "Oof, bad choice bro...",
+        badDecision: {
+            text: "Oof, bad choice bro...", destination: "Phase 3"
+        }
         // What else should go here?
     }
 }
+
+// Phase 3
 const phaseThree = {
     decisionThree: {
         goodDecision: {
             text: "You've made a good decision!", destination: "Phase 3"
         },
-        badDecision: "Oof, bad choice bro...",
+        badDecision: {
+            text: "Oof, bad choice bro...", destination: "Phase 4"
+        }
         // What else should go here?
     }
 }
+
+// Phase 4
 const phaseFour = {
     decisionFour: {
         goodDecision: {
             text: "You've made a good decision!", destination: "Phase 3"
         },
-        badDecision: "Oof, bad choice bro...",
+        badDecision: {
+            text: "Oof, bad choice bro...", destination: "Phase 5"
+        }
         // What else should go here?
     }
 }
+
+// Phase 5
 const phaseFive = {
     decisionFive: {
         goodDecision: {
             text: "You've made a good decision!", destination: "Phase 3"
         },
-        badDecision: "Oof, bad choice bro...",
+        badDecision: {
+            text: "Oof, bad choice bro...", destination: "Phase 6"
+        }
         // What else should go here?
     }
 }
